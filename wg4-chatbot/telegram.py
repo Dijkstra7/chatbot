ba48b638ea7e084a653cc0b279a2b16630e5fa2c
Dijkstra7 <rickr.dijkd@gmail.com>
2018-03-25 19:25:45 +0200

    connected to telegram, started ns api connection to do: - find way to access location? - start chatting

--- wg4-chatbot/telegram.py
+++ wg4-chatbot/telegram.py
@@ -6,10 +6,13 @@ import json
 import requests
 import time
 import urllib
+import config
+import urllib.parse
+from lxml import objectify
 # python3: urllib.parse.quote_plus
 # python2: urllib.pathname2url
-
-TOKEN = "<your-bot-token>" # don't put this in your repo! (put in config, then import config)
+TOKEN = config.TOKEN
+#TOKEN = "<your-bot-token>" # don't put this in your repo! (put in config, then import config)
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 
@@ -25,10 +28,10 @@ def get_json_from_url(url):
     return js
 
 
-def get_updates(offset=None):
-    url = URL + "getUpdates"
+def get_updates(offset=None, timeout=100):
+    url = URL + "getUpdates?timeout={}".format(timeout)
     if offset:
-        url += "?offset={}".format(offset)
+        url += "&offset={}".format(offset)
     js = get_json_from_url(url)
     return js
 
@@ -40,34 +43,52 @@ def get_last_update_id(updates):
     return max(update_ids)
 
 
-def echo_all(updates):
-    for update in updates["result"]:
-        text = update["message"]["text"]
-        chat = update["message"]["chat"]["id"]
-        send_message(text, chat)
-
-
 def get_last_chat_id_and_text(updates):
-    num_updates = len(updates["result"])
-    last_update = num_updates - 1
-    text = updates["result"][last_update]["message"]["text"]
-    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    print(len(updates["result"]))
+    last_update = - 1
+    try:
+        text = updates["result"][last_update]["message"]["text"]
+        chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    except IndexError:
+        return (None, None)
+    except Exception as e:
+        print(e)
+        return (None, None)
     return (text, chat_id)
 
 
 def send_message(text, chat_id):
-    text = urllib.pathname2url(text) # urllib.parse.quote_plus(text) # (python3)
+    text = urllib.parse.quote_plus(text) # urllib.parse.quote_plus(text) # (python3)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
 
+def echo_all(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            send_message(text, chat)
+        except Exception as e:
+            print(e)
+
+
 def main():
+    last_textchat = (None, None)
     last_update_id = None
+    auth = (config.NS_UN, config.NS_WW)
+    print("testing NS")
+    print(objectify.parse(requests.get("http://webservices.ns.nl/ns-api-avt?station=utrecht", auth=auth).content))
+    print("done testing")
     while True:
         updates = get_updates(last_update_id)
         if len(updates["result"]) > 0:
-            last_update_id = get_last_update_id(updates) + 1
-            echo_all(updates)
+            if updates["result"][-1]["message"]["text"] == "The secret testing string.":
+                print(get_json_from_url(URL+"Sendmessage?text=bla+bla&reply_markup=keyboard?KeyboardButton?text=gib+location&request_location=true"))
+            else:
+                echo_all(updates)
+            last_update_id = get_last_update_id(updates)+1
+            print([update["update_id"] for update in updates["result"]])
         time.sleep(0.5)
 
 
